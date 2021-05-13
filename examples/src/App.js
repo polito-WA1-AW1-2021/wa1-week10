2792120106c3a4415928ebd4a9d95e58baa4e60a
Fulvio Corno <fulvio.corno@polito.it>
2021-05-13 11:39:42 +0200

    examples developed in class

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './Greet';
+import { useState } from 'react' ;
+import Count from './Count';
+import QuickGate from './QuickGate';
 
 function App() {
+  const [num, setNum] = useState(3) ;
+  const [fake, setFake] = useState(3) ;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Count num={num} fake={fake}/>
+      <button onClick={()=>{ setFake(n=>n+1)}}>+</button>
+      <hr/>
+      <QuickGate/>
     </div>
   );
 }
--- flip-server/server.js
+++ flip-server/server.js
@@ -4,7 +4,19 @@ const express = require('express');
 const morgan = require('morgan') ;
 const PORT = 3001 ;
 
+const flip = require('flip-text') ;
+
 const app = express() ;
 app.use(morgan("dev"));
+
+// GET /api/flip?text=ABC
+// return JSON { text: '***' }
+// {"text":"Ɔ𐐒∀"}
+
+app.get('/api/flip', (req, res)=>{
+    const text = req.query.text ;
+    const flipped = flip(text) ;
+    res.json( { text: flipped }) ;
+} ) ;
 
 app.listen(PORT, ()=>{console.log(`Server started at http://localhost:${PORT}/`)} )
